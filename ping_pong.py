ec7af33f6161ef81e9b350ded1a94a30bbb803a0
Bobur20090909 <110126907+Bobur20090909@users.noreply.github.com>
2024-03-05 19:36:28 +0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -30,22 +30,35 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 250:
             self.rect.y += self.speed
 
-ball = GameSprite('sprites_7.png', 200,200, 100, 100, 5)
-player_l = Player("sprites_5.png", 5, 100, 40, 250, 4)
-player_r = Player("sprites_6.png", 655, 100, 40, 250, 4)
+
+ball = GameSprite('sprites_7.png', 50,50, 50, 50, 12)
+player_l = Player("sprites_5.png", 5, 50, 40, 250, 4)
+player_r = Player("sprites_6.png", 655, 50, 40, 250, 4)
 game = True
 finish = False
-while game:
-    window.blit(background,(0,0))
-    ball.reset()
-    player_l.reset()
-    player_l.update_l()
-    player_r.reset()
-    player_r.update_r()
+speed_x = 3
+speed_y = 3
 
+while game:    
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player_l, ball) or sprite.collide_rect(player_r, ball):
+            speed_x *= -1
+        
+        window.blit(background,(0,0))
+        ball.reset()
+        player_l.reset()
+        player_l.update_l()
+        player_r.reset()
+        player_r.update_r()
+
+
         # if e.type == MOUSEMOTION:
 
     display.update()
