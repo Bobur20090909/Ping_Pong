bbec3b87afdfb0e5711719948586042fa5102523
Bobur20090909 <110126907+Bobur20090909@users.noreply.github.com>
2024-03-05 18:41:49 +0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -17,12 +17,32 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
-    pass
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 250:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 250:
+            self.rect.y += self.speed
+
 ball = GameSprite('sprites_7.png', 200,200, 100, 100, 5)
+player_l = Player("sprites_5.png", 5, 100, 40, 250, 4)
+player_r = Player("sprites_6.png", 655, 100, 40, 250, 4)
 game = True
+finish = False
 while game:
     window.blit(background,(0,0))
     ball.reset()
+    player_l.reset()
+    player_l.update_l()
+    player_r.reset()
+    player_r.update_r()
+
     for e in event.get():
         if e.type == QUIT:
             game = False
